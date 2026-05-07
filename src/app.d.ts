64a18814cbc04cb6f663d1b3667eec0d8ecbe936
Suwijak Promsatid <suwijak.pak@gmail.com>
2026-05-07 21:12:01 +0700

    chore: update dependencies and improve build scripts for better performance

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,11 +1,15 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
-	namespace App {
+    namespace App {
         interface Platform {
-            env: Env
-            cf: CfProperties
-            ctx: ExecutionContext
+            env: Env;
+            cf: CfProperties;
+            ctx: ExecutionContext;
+            env: Env;
+            ctx: ExecutionContext;
+            caches: CacheStorage;
+            cf?: IncomingRequestCfProperties
         }
     }
 }
